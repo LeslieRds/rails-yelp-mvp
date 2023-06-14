a831c108f9c3cb3f22e8d754131d066be314c3f8
Leslie Rados <leslie.rados@gmail.com>
2023-06-14 12:47:36 +0200

    pvm pley sliar 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+  Restaurant.create!(
+    name: Faker::Fantasy::Tolkien.location,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+end
